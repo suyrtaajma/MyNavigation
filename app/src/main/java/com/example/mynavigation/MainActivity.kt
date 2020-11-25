d54a48b603b01227ac91f1ac67e7595338184cf2
Bakti Surya Atmaja <55836689+suyrtaajma@users.noreply.github.com>
2020-11-25 09:45:16 +0700

    first commit

---
+++ app/src/main/java/com/example/mynavigation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mynavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.picodiploma.mynavigation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
